8c1258e87ba78f614c97eb074ffeb00167ef6f57
fadhilachmadi <fadhilachmadi@gmail.com>
2020-09-03 21:32:05 +0700

    add intent

--- app/src/main/java/com/example/exercise1/MainActivity.kt
+++ app/src/main/java/com/example/exercise1/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.exercise1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        arrow.setOnClickListener {
+            openFirstActivity()
+        }
     }
+
+    private fun openFirstActivity(){
+        val intent = Intent(this, FirstActivity::class.java).apply {
+            putExtra("Extra", "This is from main activity")
+        }
+        startActivity(intent)
+    }
+
+
 }
